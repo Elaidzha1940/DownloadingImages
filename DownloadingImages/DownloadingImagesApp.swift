22f4cbf33814d4fd7016a3c32e2ea7966309f7c2
Elaidzha1940 <eli080293@gmail.com>
2023-11-13 17:27:05 +0300

    DownloadingImages

---
+++ DownloadingImages/DownloadingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadingImagesApp.swift
+//  DownloadingImages
+//
+//  Created by Elaidzha Shchukin on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DownloadingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
