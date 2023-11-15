6f2a401bcfeb55287278c5d1af0e835ce2624750
Elaidzha1940 <eli080293@gmail.com>
2023-11-15 19:44:08 +0300

    Update PhtotModelDataService

--- DownloadingImages/Utilities/PhtotModelDataService.swift
+++ DownloadingImages/Utilities/PhtotModelDataService.swift
@@ -12,20 +12,41 @@ import Foundation
 class PhtotModelDataService {
     
     static let instance = PhtotModelDataService() // Singleton
+    
+    @Published var photoModels: [PhotoModel] = []
+     
     private init() {
         
     }
     
     func downloadData() {
         
-        guard let url = 
+        guard let url =
                 URL(string: "https://jsonplaceholder.typicode.com/photos") else { return }
         
         URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .background))
             .receive(on: DispatchQueue.main)
-            .tryMap(<#T##transform: (Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>.Output) throws -> T##(Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>.Output) throws -> T#>)
+            .tryMap(handleOutput)
+            .decode(type: [PhotoModel].self, decoder: JSONDecoder())
+            .sink { (complition) in
+                switch complition {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error downloding data. \(error)")
+                }
+            } receiveValue: { [weak self] (returnedPhotoModels) in
+                self?.photoModels = returnedPhotoModels
+            }
+
         
+    }
+    private func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
         }
+        return output.data
     }
 }
