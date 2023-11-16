a0b8a8d3b91bf6e60fef64c5e7d1d836820169f4
Elaidzha1940 <eli080293@gmail.com>
2023-11-16 05:14:29 +0300

    Update PhotoModelFileManager

--- DownloadingImages/Utilities/PhotoModelFileManager.swift
+++ DownloadingImages/Utilities/PhotoModelFileManager.swift
@@ -20,7 +20,16 @@ class PhotoModelFileManager {
 
     
     private func createFolderIfNeeded() {
+        guard let url = getFolderPath() else { return }
         
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+                print("Created folder.")
+            } catch let error {
+                print("Error creating folder. \(error)")
+            }
+        }
     }
     
     private func getFolderPath() -> URL? {
@@ -29,5 +38,38 @@ class PhotoModelFileManager {
             .urls(for: .cachesDirectory , in: .userDomainMask)
             .first?
             .appendingPathComponent(folderName)
+    }
+    
+    //  ... /downloaded_images/
+    //  ... /downloaded_images/image_name.png
+    
+    private func getImagesPath(key: String) -> URL? {
+        guard let folder = getFolderPath() else {
+            return nil
+        }
+        return folder.appendingPathComponent(key + ".png")
+    }
+    
+    func add(key: String, value: UIImage) {
+        
+        guard
+            let data = value.pngData(),
+            let url = getImagesPath(key: key) else { return }
+        
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving to file manager. \(error)")
+        }
+    }
+    
+    func get(key: String) -> UIImage? {
+        
+        guard
+            let url = getImagesPath(key: key),
+            FileManager.default.fileExists(atPath: url.path) else {
+            return nil
+        }
+        return UIImage(contentsOfFile: url.path)
     }
 }
