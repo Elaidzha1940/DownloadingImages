927b5585753aaae76e758a971296ddd5677282b1
Elaidzha1940 <eli080293@gmail.com>
2023-11-16 03:34:17 +0300

    UpdateImageLoadingViewModel

--- DownloadingImages/ViewModels/ImageLoadingViewModel.swift
+++ DownloadingImages/ViewModels/ImageLoadingViewModel.swift
@@ -15,10 +15,26 @@ class ImageLoadingViewModel: ObservableObject {
     @Published var image: UIImage? = nil
     @Published var isLoading: Bool = false
     
+    init() {
+        downloadImage()
+    }
+    
     func downloadImage() {
         
-        guard let url = URL(string: "") else { return }
+        isLoading = true
+        guard let url = URL(string: "") else {
+            isLoading = false
+            return
+        }
         
         URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+
     }
 }
